32ff5d717248988921c1b11eb32634f6c3028f28
Alan <alan.carusoo@gmail.com>
2025-02-20 21:35:00 -0300

    feat: added category deletion support in controller and routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -18,3 +18,15 @@ exports.postCategories = async (req, res) => {
     res.status(500).json({ error: "Error creating categories" });
   }
 };
+
+exports.deleteCategories = async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json({ message: "Category deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting Category", error });
+  }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,5 +4,6 @@ const categoryController = require("../controllers/categoryController");
 
 router.get("/", categoryController.getCategories);
 router.post("/", categoryController.postCategories);
+router.delete("/:id", categoryController.deleteCategories);
 
 module.exports = router;
